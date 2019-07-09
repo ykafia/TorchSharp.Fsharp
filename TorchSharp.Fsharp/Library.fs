9a942efd07b10eeaaf14a895279103fb16484efd
Youness <kafia.youness@gmail.com>
2019-07-09 11:41:38 +0200

    Initial Commit

---
+++ TorchSharp.Fsharp/Library.fs
@@ -0,0 +1,14 @@
+﻿namespace TorchSharp.FSharp
+open TorchSharp
+open TorchSharp.Tensor
+open TorchSharp.NN
+module Tuple2 = 
+  let map f (a, b) = (f a, f b)
+module FsModule =
+    
+    let Forward  y (x:ProvidedModule) = x.Forward(y)
+    
+    let MaxPool2D (kernelSize:list<int64>) (stride:list<int64>) (tensor:TorchTensor)= 
+        (kernelSize,stride) 
+            |> Tuple2.map List.toArray 
+            |> NN.Module.MaxPool2D
